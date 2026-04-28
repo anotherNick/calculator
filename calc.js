231ceeed5cf4386545ba8afdf636eb4936f70067
anotherNick <nick.steimling@gmail.com>
2026-04-28 04:27:23 -0500

    Make buttons clickable.
    Added click event to buttons so we can begin figuring out
    what to do with user selections.

--- calc.js
+++ calc.js
@@ -1,3 +1,7 @@
+let a;
+let b;
+let op;
+
 function operate(a, b, op){
 
     switch(op){
@@ -14,11 +18,29 @@ function operate(a, b, op){
             return a / b;
             break;
         default:
-            alert('Invalid operator specified!');
+            alert('Invalid operator specified!')
     }
 
 }
 
+function updateNumber(button){
+    console.log(button);
+}
 
+const numberButtons = document.getElementById("number-buttons");
+
+numberButtons.addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON'){
+        updateNumber(e.target.innerText);
+    }
+});
+
+const operatorButtons = document.getElementById("operator-buttons");
+
+operatorButtons.addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON'){
+        updateNumber(e.target.innerText);
+    }
+})
 
 
