f6f406dfc39f26afa346cf59c7b392221e2c17bc
anotherNick <nick.steimling@gmail.com>
2026-04-28 17:51:40 -0500

    Add logic to update with user input.
    updateNumber now modifies the correct variable depending on whether
    there was already an operator and hands it off to updateDisplay.

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
-let a;
-let b;
-let op;
+let a = null;
+let b = null;
+let op = null;
 
 function operate(a, b, op){
 
@@ -23,8 +23,62 @@ function operate(a, b, op){
 
 }
 
-function updateNumber(button){
-    console.log(button);
+function updateDisplay(str){
+
+    const display = document.getElementById('display');
+
+    display.innerText = str;
+
+}
+
+function updateNumber(value){
+
+    switch(value){
+        case ".":
+            addDecimal();
+            break;
+        case "=":
+            operate(a, b, op);
+            break;
+    }
+
+    if(op === null && a === null){
+
+        a = value;
+        updateDisplay(a);
+
+    }else if(op === null){
+
+        a += value;
+        updateDisplay(a)
+
+    }else if(op !== null && b === null){
+        
+        b = value;
+        updateDisplay(a + op + b);
+
+    }else if(op !== null){
+
+        b += value;
+        updateDisplay(a + op + b);
+
+    }
+
+}
+
+function updateOperator(value){
+
+    if(op === null || b === null){
+
+        op = value;
+        updateDisplay(a + op);
+    
+    }else{
+
+        
+
+    }
+
 }
 
 const numberButtons = document.getElementById("number-buttons");
@@ -39,7 +93,7 @@ const operatorButtons = document.getElementById("operator-buttons");
 
 operatorButtons.addEventListener('click', (e) => {
     if(e.target.tagName === 'BUTTON'){
-        updateNumber(e.target.innerText);
+        updateOperator(e.target.innerText);
     }
 })
 
